3d012af59f71bad77bd5e6ea1b7436ae748057f5
tnfo222 <tayforbes92@hotmail.com>
2022-10-23 13:32:57 -0400

    Activity completed

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,22 @@
 # code your iterative algorithm here
 
+def binary_search (data, e1):
+    first = 0
+    last = len(data) - 1
+    found = False
+
+    while first <= last and not found:
+        mid = (first + last) // 2
+
+        if data[mid] == e1:
+            found = True
+        else:
+            if e1 < data[mid]:
+                last = mid + 1
+            else:
+                first = mid + 1
+    return found
+
+test_list = [5,8,12,14,19,22,27,30,31]
+
+print(binary_search(test_list,100)) 
